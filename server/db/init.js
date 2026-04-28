a86ac04325b2a27f52494c9d688971653e1c883c
adr-codes <admedina335@gmail.com>
2026-04-28 17:04:55 +0800

    Fix database path for Render

--- server/db/init.js
+++ server/db/init.js
@@ -1,7 +1,9 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const dbPath = path.join(__dirname, 'barangay.db');
+const dbPath = process.env.RENDER 
+  ? '/opt/render/project/src/server/db/barangay.db' 
+  : path.join(__dirname, 'barangay.db');
 const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
