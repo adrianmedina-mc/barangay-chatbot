4a93896ae0389050e7355a9d43aa285e5d0d5a79
adrianmedina-mc <adrianmedina@mabinicolleges.edu.ph>
2026-05-30 01:56:23 +0800

    fix

--- server/routes/residents.js
+++ server/routes/residents.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/init');
 const authMiddleware = require('../middleware/auth');
-const { sendMessage } = require('../services/messenger');
 const { sendMessage, sendQuickReplies } = require('../services/messenger');
 
 router.use(authMiddleware);
